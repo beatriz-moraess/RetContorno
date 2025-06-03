9d203183c6737ca87274029f5d21fe27e45d7cc9
Beatriz <beatriz.giovanna@gmail.com>
2025-06-02 22:33:11 -0300

    finaliza programa

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Clear();
+
+int altura = 0;
+int largura = 0;
+
+Console.Write("Olá, para fazer o retângulo digite a altura: ");
+altura = Convert.ToInt32(Console.ReadLine()!);
+
+Console.Write("Agora, digite a largura: ");
+largura = Convert.ToInt32(Console.ReadLine()!);
+
+for (int i = 0; i < altura; i++)
+{
+    for (int j = 0; j < largura; j++)
+    {
+        if ( i == 0 || j == 0  || i == altura - 1 || j == largura - 1)
+        {
+            Console.Write("*");
+        }
+        else
+        {
+            Console.Write(" ");
+        }
+    }
+            
+    Console.WriteLine();
+}
+
+    
